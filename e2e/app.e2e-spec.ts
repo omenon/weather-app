d33ee3ede9ba42b4ed5bd659dcfcdeb8e576b710
Angular CLI <angular-cli@angular.io>
2018-02-15 22:59:06 +0200

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('weater App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to wa!');
+  });
+});
