dc256405a70febdd9ad4a87c5bc844ceed3d4aa7
Ivan kovalchuk <omenon1995@gmail.com>
2018-02-16 00:03:22 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
